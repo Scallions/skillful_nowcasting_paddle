ee0b555bd558ef3986b3b87a3499cb106859f63a
yifwahaha <940503449@qq.com>
2022-05-11 23:13:23 +0800

    add eval

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import paddle
 import os
+import numpy as np
 from logger import logger_config
 import paddle.optimizer
 from paddle.io import DataLoader
@@ -16,7 +17,7 @@ logger = logger_config(log_path='result/DGMR_log.txt', logging_name='DGMR')
 G = Generator(num_channels=1, lead_time=90, time_delta=5)
 D = Discriminator(input_channel=1)
 
-opt_G = paddle.optimizer.Adam(5e-5,parameters=G.parameters(), beta1=0.0, beta2=0.999)
+opt_G = paddle.optimizer.Adam(5e-5, parameters=G.parameters(), beta1=0.0, beta2=0.999)
 opt_D = paddle.optimizer.Adam(2e-4, parameters=D.parameters(), beta1=0.0, beta2=0.999)
 
 PATH = r'E:\dataset\pwv.nc'
@@ -39,6 +40,7 @@ test_loader = DataLoader(test_dataset,
 
 TOTAL_EPOCH = 100
 TOTAL_STEP = len(train_loader)
+
 for epoch in range(TOTAL_EPOCH):
     #ds = [(paddle.rand([2,22,1,256,256]), paddle.rand([2,22,1,256,256]))]
     for step, (inp, target) in enumerate(train_loader):
@@ -83,5 +85,42 @@ for epoch in range(TOTAL_EPOCH):
                                                                                              TOTAL_STEP,
                                                                                              disc_loss.item(),
                                                                                              gen_loss.item()))
+
+    if epoch % 2 == 0:
+        disc_loss_list = []
+        gen_loss_list = []
+        with paddle.no_grad():
+            for _, (inp, target) in test_loader:
+                inp = inp.astype(paddle.float32)
+                target = target.astype(paddle.float32)
+                pred = G(inp)
+                gen_seq = paddle.concat([inp, pred], axis=1)
+                real_seq = paddle.concat([inp, target], axis=1)
+                concat_inps = paddle.concat([real_seq, gen_seq], axis=0)
+                concat_outs = D(concat_inps)
+                score_real, score_gen = paddle.split(concat_outs, num_or_sections=2, axis=0)
+
+                disc_loss = loss_hinge_disc(score_gen, score_real)
+
+                num_samples = 6
+                gen_samples = [
+                    G(inp) for _ in range(num_samples)
+                ]
+                grid_cell_reg = grid_cell_regularizer(paddle.stack(gen_samples, axis=0), target)
+                gen_seqs = [
+                    paddle.concat([inp, gen_sample], axis=1) for gen_sample in gen_samples
+                ]
+                gen_disc_loss = loss_hinge_gen(D(paddle.concat(gen_seqs, axis=0)))
+                gen_loss = gen_disc_loss + grid_cell_reg
+
+                disc_loss_list.append(disc_loss.item())
+                gen_loss_list.append(gen_loss.item())
+
+        test_disc_loss = np.mean(disc_loss_list)
+        test_gen_loss = np.mean(gen_loss_list)
+        logger.info('[EVAL] Epoch [{}/{}], d_loss: {:.4f}, g_loss: {:.4f}'.format(epoch,
+                                                                                  TOTAL_EPOCH,
+                                                                                  test_disc_loss,
+                                                                                  test_gen_loss))
 
 
