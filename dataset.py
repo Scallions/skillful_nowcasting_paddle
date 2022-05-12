29805a16e41e416148cd90664f1fa3988bce24be
Scallions_ <844120806@qq.com>
2022-05-12 16:29:17 +0800

    数据集换成xarray读取，添加VDL

--- dataset.py
+++ dataset.py
@@ -1,57 +1,15 @@
 import paddle
-import netCDF4
+import xarray as xr
 import numpy as np
 from paddle.io import Dataset, BatchSampler, DataLoader
-
-
-# PATH = r'E:\dataset\pwv.nc'
-# nc_obj = netCDF4.Dataset(PATH)
-# print(nc_obj)
-# print('---------------------------------------')
-#
-# #查看nc文件中的变量
-# print(nc_obj.variables.keys())
-# for i in nc_obj.variables.keys():
-#     print(i)
-# print('---------------------------------------')
-#
-# #查看每个变量的信息
-# print(nc_obj.variables['time'])
-# print(nc_obj.variables['latitude'])
-# print(nc_obj.variables['longitude'])
-# print(nc_obj.variables['__xarray_dataarray_variable__'])
-# print('---------------------------------------')
-#
-# #查看每个变量的属性
-# print(nc_obj.variables['time'].ncattrs())
-# print(nc_obj.variables['latitude'].ncattrs())
-# print(nc_obj.variables['longitude'].ncattrs())
-# print(nc_obj.variables['__xarray_dataarray_variable__'].ncattrs())
-# print('---------------------------------------')
-#
-# #读取数据值
-# time=(nc_obj.variables['time'][:])
-# lon=(nc_obj.variables['latitude'][:])
-# lat=(nc_obj.variables['longitude'][:])
-# value = (nc_obj.variables['__xarray_dataarray_variable__'][0])
-# print('row',value.shape[0])
-# print('col',value.shape[1])
-# print(time)
-# print(lon)
-# print('---------------******-------------------')
-# print(lat)
-# print('---------------******-------------------')
-# print(value.shape)
-# print(value)
 
 class NowCastingDataset(Dataset):
     def __init__(self, path, length, ratio, training):
         super().__init__()
-        nc_obj = netCDF4.Dataset(path)
-        self.value = nc_obj.variables['__xarray_dataarray_variable__']
-        self.row = nc_obj.variables['__xarray_dataarray_variable__'].shape[1]
-        self.col = nc_obj.variables['__xarray_dataarray_variable__'].shape[2]
-        self.total_data = nc_obj.variables['__xarray_dataarray_variable__'].shape[0]
+        self.value = xr.load_dataarray(path)
+        self.lats = np.linspace(self.value.latitude[0], self.value.latitude[-1], 256)
+        self.lons = np.linspace(self.value.longitude[0], self.value.longitude[-1], 256)
+        self.total_data = self.value.shape[0]
         self.ratio = int(self.total_data * ratio)
         self.length = length
         self.length_target = 4
@@ -64,8 +22,12 @@ class NowCastingDataset(Dataset):
             data = self.value[self.ratio:]
         inp = data[item:item + self.length]
         tar = data[item + self.length:item + self.length + self.length_target]
-        input = np.resize(np.reshape(inp, (self.length, 1, self.row, self.col)), (self.length, 1, 256, 256))
-        target = np.resize(np.reshape(tar, (self.length_target, 1, self.row, self.col)), (self.length_target, 1, 256, 256))
+        # input = np.resize(np.reshape(inp, (self.length, 1, self.row, self.col)), (self.length, 1, 256, 256))
+        # target = np.resize(np.reshape(tar, (self.length_target, 1, self.row, self.col)), (self.length_target, 1, 256, 256))
+        input = inp.interp(longitude=self.lons, latitude=self.lats).values
+        target = tar.interp(longitude=self.lons, latitude=self.lats).values
+        input = np.reshape(input, (self.length, 1, 256, 256))
+        target = np.reshape(target, (self.length_target, 1, 256, 256))
         return [input, target]
 
     def __len__(self):
--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 from datetime import datetime
 from logger import logger_config
+from visualdl import LogWriter
 from tools import save_model
 import paddle.optimizer
 from paddle.io import DataLoader
@@ -16,7 +17,7 @@ current_time = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
 if not os.path.isdir('result'):
     os.makedirs('result')
 logger = logger_config(log_path='result/DGMR_'+current_time+'.txt', logging_name='DGMR')
-
+writer = LogWriter(log_dir='result/vd')
 
 G = Generator(num_channels=1, lead_time=90, time_delta=5)
 D = Discriminator(input_channel=1)
@@ -24,9 +25,9 @@ D = Discriminator(input_channel=1)
 opt_G = paddle.optimizer.Adam(5e-5, parameters=G.parameters(), beta1=0.0, beta2=0.999)
 opt_D = paddle.optimizer.Adam(2e-4, parameters=D.parameters(), beta1=0.0, beta2=0.999)
 
-PATH = r'E:\dataset\pwv.nc'
-LENGTH = 10
-BATCH_SIZE = 4
+PATH = r'/home/aistudio/data/data144877/pwv.nc'
+LENGTH = 18
+BATCH_SIZE = 1
 
 train_dataset = NowCastingDataset(PATH, LENGTH, 0.8, training=True)
 train_loader = DataLoader(train_dataset,
@@ -44,11 +45,13 @@ test_loader = DataLoader(test_dataset,
 
 TOTAL_EPOCH = 100
 TOTAL_STEP = len(train_loader)
+all_step = 0
 
 for epoch in range(TOTAL_EPOCH):
     #ds = [(paddle.rand([2,22,1,256,256]), paddle.rand([2,22,1,256,256]))]
     for step, (inp, target) in enumerate(train_loader):
     #for inp, target in ds:
+        all_step += 1
         # b t c h w
         inp = inp.astype(paddle.float32)
         target = target.astype(paddle.float32)
@@ -89,44 +92,64 @@ for epoch in range(TOTAL_EPOCH):
                                                                                              TOTAL_STEP,
                                                                                              disc_loss.item(),
                                                                                              gen_loss.item()))
+            writer.add_scalar(tag="train/ld", step=all_step, value=disc_loss.numpy().item())
+            writer.add_scalar(tag="train/lg", step=all_step, value=gen_loss.numpy().item())
+        if all_step % 1000 == 0:
+            # eval
+            D.eval()
+            G.eval()
+            disc_loss_list = []
+            gen_loss_list = []
+            with paddle.no_grad():
+                for inp, target in test_loader:
+                    inp = inp.astype(paddle.float32)
+                    target = target.astype(paddle.float32)
+                    pred = G(inp)
+                    gen_seq = paddle.concat([inp, pred], axis=1)
+                    real_seq = paddle.concat([inp, target], axis=1)
+                    concat_inps = paddle.concat([real_seq, gen_seq], axis=0)
+                    concat_outs = D(concat_inps)
+                    score_real, score_gen = paddle.split(concat_outs, num_or_sections=2, axis=0)
+
+                    disc_loss = loss_hinge_disc(score_gen, score_real)
+
+                    num_samples = 6
+                    gen_samples = [
+                        G(inp) for _ in range(num_samples)
+                    ]
+                    grid_cell_reg = grid_cell_regularizer(paddle.stack(gen_samples, axis=0), target)
+                    gen_seqs = [
+                        paddle.concat([inp, gen_sample], axis=1) for gen_sample in gen_samples
+                    ]
+                    gen_disc_loss = loss_hinge_gen(D(paddle.concat(gen_seqs, axis=0)))
+                    gen_loss = gen_disc_loss + grid_cell_reg
+
+                    disc_loss_list.append(disc_loss.item())
+                    gen_loss_list.append(gen_loss.item())
+                    break
+
+            test_disc_loss = np.mean(disc_loss_list)
+            test_gen_loss = np.mean(gen_loss_list)
+            logger.info('[EVAL] Epoch [{}/{}], d_loss: {:.4f}, g_loss: {:.4f}'.format(epoch,
+                                                                                    TOTAL_EPOCH,
+                                                                                    test_disc_loss,
+                                                                                    test_gen_loss))
+            writer.add_scalar(tag="test/ld", step=all_step, value=test_disc_loss.item())
+            writer.add_scalar(tag="test/lg", step=all_step, value=test_gen_loss.item())
+            writer.add_image(tag="predict",
+                                img=pred[0,0,0,:,:].detach().numpy(),
+                                step=step,
+                                dataformats="HW",
+                                )
+            writer.add_image(tag="true",
+                                img=target[0,0,0,:,:].detach().numpy(),
+                                step=step,
+                                dataformats="HW",
+                                )
+            D.train()
+            G.train()
     save_model(G, opt_G, 'model', epoch, 'G')
     save_model(D, opt_D, 'model', epoch, 'D')
 
-    if epoch % 2 == 0:
-        disc_loss_list = []
-        gen_loss_list = []
-        with paddle.no_grad():
-            for _, (inp, target) in test_loader:
-                inp = inp.astype(paddle.float32)
-                target = target.astype(paddle.float32)
-                pred = G(inp)
-                gen_seq = paddle.concat([inp, pred], axis=1)
-                real_seq = paddle.concat([inp, target], axis=1)
-                concat_inps = paddle.concat([real_seq, gen_seq], axis=0)
-                concat_outs = D(concat_inps)
-                score_real, score_gen = paddle.split(concat_outs, num_or_sections=2, axis=0)
-
-                disc_loss = loss_hinge_disc(score_gen, score_real)
-
-                num_samples = 6
-                gen_samples = [
-                    G(inp) for _ in range(num_samples)
-                ]
-                grid_cell_reg = grid_cell_regularizer(paddle.stack(gen_samples, axis=0), target)
-                gen_seqs = [
-                    paddle.concat([inp, gen_sample], axis=1) for gen_sample in gen_samples
-                ]
-                gen_disc_loss = loss_hinge_gen(D(paddle.concat(gen_seqs, axis=0)))
-                gen_loss = gen_disc_loss + grid_cell_reg
-
-                disc_loss_list.append(disc_loss.item())
-                gen_loss_list.append(gen_loss.item())
-
-        test_disc_loss = np.mean(disc_loss_list)
-        test_gen_loss = np.mean(gen_loss_list)
-        logger.info('[EVAL] Epoch [{}/{}], d_loss: {:.4f}, g_loss: {:.4f}'.format(epoch,
-                                                                                  TOTAL_EPOCH,
-                                                                                  test_disc_loss,
-                                                                                  test_gen_loss))
-
+        # if step % 2 == 0:
 
