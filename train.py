01bd1adc3078fd89865ee02ef209718ab9e8aced
yifwahaha <940503449@qq.com>
2022-05-13 16:19:43 +0800

    修改模型保存间隔

--- train.py
+++ train.py
@@ -12,11 +12,10 @@ from models.generators import Generator
 from models.discriminators import Discriminator
 from models.modules.loss import loss_hinge_disc, loss_hinge_gen, grid_cell_regularizer
 
-
 current_time = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
 if not os.path.isdir('result'):
     os.makedirs('result')
-logger = logger_config(log_path='result/DGMR_'+current_time+'.txt', logging_name='DGMR')
+logger = logger_config(log_path='result/DGMR_' + current_time + '.txt', logging_name='DGMR')
 writer = LogWriter(log_dir='result/vd')
 
 G = Generator(num_channels=1, lead_time=90, time_delta=5)
@@ -31,26 +30,26 @@ BATCH_SIZE = 1
 
 train_dataset = NowCastingDataset(PATH, LENGTH, 0.8, training=True)
 train_loader = DataLoader(train_dataset,
-                    batch_size=BATCH_SIZE,
-                    shuffle=False,
-                    drop_last=True,
-                    num_workers=4) # if windows, num_workers must be set 0
+                          batch_size=BATCH_SIZE,
+                          shuffle=False,
+                          drop_last=True,
+                          num_workers=4)  # if windows, num_workers must be set 0
 
 test_dataset = NowCastingDataset(PATH, LENGTH, 0.8, training=False)
 test_loader = DataLoader(test_dataset,
-                    batch_size=BATCH_SIZE,
-                    shuffle=False,
-                    drop_last=True,
-                    num_workers=4)
+                         batch_size=BATCH_SIZE,
+                         shuffle=False,
+                         drop_last=True,
+                         num_workers=4)
 
 TOTAL_EPOCH = 100
 TOTAL_STEP = len(train_loader)
 all_step = 0
 
 for epoch in range(TOTAL_EPOCH):
-    #ds = [(paddle.rand([2,22,1,256,256]), paddle.rand([2,22,1,256,256]))]
+    # ds = [(paddle.rand([2,22,1,256,256]), paddle.rand([2,22,1,256,256]))]
     for step, (inp, target) in enumerate(train_loader):
-    #for inp, target in ds:
+        # for inp, target in ds:
         all_step += 1
         # b t c h w
         inp = inp.astype(paddle.float32)
@@ -87,13 +86,18 @@ for epoch in range(TOTAL_EPOCH):
 
         if step % 100 == 0:
             logger.info('[TRAIN] Epoch [{}/{}], Step [{}/{}], d_loss: {:.4f}, g_loss: {:.4f}'.format(epoch,
-                                                                                             TOTAL_EPOCH,
-                                                                                             step,
-                                                                                             TOTAL_STEP,
-                                                                                             disc_loss.item(),
-                                                                                             gen_loss.item()))
+                                                                                                     TOTAL_EPOCH,
+                                                                                                     step,
+                                                                                                     TOTAL_STEP,
+                                                                                                     disc_loss.item(),
+                                                                                                     gen_loss.item()))
             writer.add_scalar(tag="train/ld", step=all_step, value=disc_loss.numpy().item())
             writer.add_scalar(tag="train/lg", step=all_step, value=gen_loss.numpy().item())
+
+        if all_step % 2000 == 0:
+            save_model(G, opt_G, 'model', epoch, all_step, 'G')
+            save_model(D, opt_D, 'model', epoch, all_step, 'D')
+
         if all_step % 1000 == 0:
             # eval
             D.eval()
@@ -126,30 +130,25 @@ for epoch in range(TOTAL_EPOCH):
 
                     disc_loss_list.append(disc_loss.item())
                     gen_loss_list.append(gen_loss.item())
-                    break
 
             test_disc_loss = np.mean(disc_loss_list)
             test_gen_loss = np.mean(gen_loss_list)
             logger.info('[EVAL] Epoch [{}/{}], d_loss: {:.4f}, g_loss: {:.4f}'.format(epoch,
-                                                                                    TOTAL_EPOCH,
-                                                                                    test_disc_loss,
-                                                                                    test_gen_loss))
+                                                                                      TOTAL_EPOCH,
+                                                                                      test_disc_loss,
+                                                                                      test_gen_loss))
             writer.add_scalar(tag="test/ld", step=all_step, value=test_disc_loss.item())
             writer.add_scalar(tag="test/lg", step=all_step, value=test_gen_loss.item())
             writer.add_image(tag="predict",
-                                img=pred[0,0,0,:,:].detach().numpy(),
-                                step=step,
-                                dataformats="HW",
-                                )
+                             img=pred[0, 0, 0, :, :].detach().numpy(),
+                             step=step,
+                             dataformats="HW",
+                             )
             writer.add_image(tag="true",
-                                img=target[0,0,0,:,:].detach().numpy(),
-                                step=step,
-                                dataformats="HW",
-                                )
+                             img=target[0, 0, 0, :, :].detach().numpy(),
+                             step=step,
+                             dataformats="HW",
+                             )
             D.train()
             G.train()
-    save_model(G, opt_G, 'model', epoch, 'G')
-    save_model(D, opt_D, 'model', epoch, 'D')
-
-        # if step % 2 == 0:
 
